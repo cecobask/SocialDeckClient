1e92eb4db1632a6a84dea8baffe0339db52440e1
Tsvetoslav Dimov <baskski@gmail.com>
2019-12-05 18:18:11 +0000

    Introduced GitLab for Continuous Integration, Deployment and Delivery. Deployment target - AWS S3.

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   chainWebpack: config => {
-    // GraphQL Loader
     config.module
       .rule('graphql')
       .test(/\.graphql$/)
       .use('graphql-tag/loader')
       .loader('graphql-tag/loader')
       .end()
+    config.performance
+      .hints(false)
   }
 }
